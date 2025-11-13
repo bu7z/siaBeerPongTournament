15de04c56489fce22e389f44560d51aa9ae5a928
Butzke <bu7z@bu7z-T.fritz.box>
2025-11-13 18:49:29 +0100

    yeetv2

--- beerpong/frontend/vite.config.js
+++ beerpong/frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  server: {
+    allowedHosts: [
+      'butzke.it'
+    ],
+    host: true,        // wichtig, wenn du von außen zugreifst
+    port: 5173,        // oder dein custom-Port
+  }
 })
